d61f3ccb120f5ac188b67f41ba68da55ca08a895
Carlos Figueira <carlosfigueirafilho@hotmail.com>
2011-10-17 13:39:37 -0700

    A few more entries

--- WCFForums/QuickCode1/Program.cs
+++ WCFForums/QuickCode1/Program.cs
@@ -16393,11 +16393,124 @@ namespace QuickCode1
         }
     }
 
+    // http://stackoverflow.com/q/7771645/751090
+    public class StackOverflow_7771645
+    {
+        [ServiceContract]
+        public interface ITest
+        {
+            [OperationContract]
+            string Process();
+        }
+        public class Service : ITest
+        {
+            public string Process()
+            {
+                return "Request content type: " + WebOperationContext.Current.IncomingRequest.ContentType;
+            }
+        }
+        public static void Test()
+        {
+            string baseAddress = "http://" + Environment.MachineName + ":8000/Service";
+            WebServiceHost host = new WebServiceHost(typeof(Service), new Uri(baseAddress));
+            host.Open();
+            Console.WriteLine("Host opened");
+
+            WebChannelFactory<ITest> factory = new WebChannelFactory<ITest>(new Uri(baseAddress));
+            ITest proxy = factory.CreateChannel();
+            using (new OperationContextScope((IContextChannel)proxy))
+            {
+                WebOperationContext.Current.OutgoingRequest.ContentType = "text/xml";
+                Console.WriteLine(proxy.Process());
+            }
+
+            using (new OperationContextScope((IContextChannel)proxy))
+            {
+                WebOperationContext.Current.OutgoingRequest.ContentType = "application/xml";
+                Console.WriteLine(proxy.Process());
+            }
+
+            ((IClientChannel)proxy).Close();
+            factory.Close();
+
+            Console.Write("Press ENTER to close the host");
+            Console.ReadLine();
+            host.Close();
+        }
+    }
+
+    // http://stackoverflow.com/q/7760551/751090
+    public class StackOverflow_7760551
+    {
+        [DataContract]
+        public class Person
+        {
+            [DataMember]
+            public string Name { get; set; }
+            [DataMember]
+            public int Age { get; set; }
+
+            public override string ToString()
+            {
+                return string.Format("Person[Name={0},Age={1}]", this.Name, this.Age);
+            }
+        }
+
+        public static void Test()
+        {
+            const string fileName = "test.xml";
+            using (FileStream fs = File.Create(fileName))
+            {
+                Person[] people = new Person[]
+                { 
+                    new Person { Name = "John", Age = 33 },
+                    new Person { Name = "Jane", Age = 28 },
+                    new Person { Name = "Jack", Age = 23 }
+                };
+
+                foreach (Person p in people)
+                {
+                    XmlWriterSettings ws = new XmlWriterSettings
+                    {
+                        Indent = true,
+                        IndentChars = "  ",
+                        OmitXmlDeclaration = true,
+                        Encoding = new UTF8Encoding(false),
+                        CloseOutput = false,
+                    };
+                    using (XmlWriter w = XmlWriter.Create(fs, ws))
+                    {
+                        DataContractSerializer dcs = new DataContractSerializer(typeof(Person));
+                        dcs.WriteObject(w, p);
+                    }
+                }
+            }
+
+            Console.WriteLine(File.ReadAllText(fileName));
+
+            using (FileStream fs = File.OpenRead(fileName))
+            {
+                XmlReaderSettings rs = new XmlReaderSettings
+                {
+                    ConformanceLevel = ConformanceLevel.Fragment,
+                };
+                XmlReader r = XmlReader.Create(fs, rs);
+                while (!r.EOF)
+                {
+                    Person p = new DataContractSerializer(typeof(Person)).ReadObject(r) as Person;
+                    Console.WriteLine(p);
+                }
+            }
+
+            File.Delete(fileName);
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Post_9bf860f1_2a01_4b26_8c79_2f6346191dc9.Test();
+            StackOverflow_7760551.Test();
         }
     }
 }
