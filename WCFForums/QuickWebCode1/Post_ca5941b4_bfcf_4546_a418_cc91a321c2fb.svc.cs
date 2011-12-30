0a615b249c32b2aa586ec479092edfe67ed07d29
Carlos Figueira <carlosfigueirafilho@hotmail.com>
2011-12-30 07:46:31 -0800

    Added missing files

---
+++ WCFForums/QuickWebCode1/Post_ca5941b4_bfcf_4546_a418_cc91a321c2fb.svc.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Post_ca5941b4_bfcf_4546_a418_cc91a321c2fb
+{
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        int Add(int x, int y);
+    }
+    public class Service1 : IService1
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
